e2a3c418e7a29bd290a52ca9a941f20d4a9c6d3a
Erik <edo0005@uah.edu>
2022-10-24 02:03:51 -0500

    commented code

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,14 @@ import java.util.Scanner;
 import static java.lang.Math.*;
 
 public class Main {
-
+    /**
+     * Compares the values of strings to the chosen pivot value until i > pivot and j < pivot, then
+     * switches i and j
+     * @param arr array of strings
+     * @param lo lower bound of array
+     * @param hi upper bound of array
+     * @return returns index of partition
+     */
     public static int myPartition(String[] arr, int lo, int hi) {
         String pivotValue = arr[lo];
         //char[] pivotArray = pivotValue.toLowerCase().toCharArray();
@@ -38,6 +45,13 @@ public class Main {
         arr[lo] = temp;
         return j;
     }
+
+    /**
+     * Partitions the array into halves and recursively calls itself to perform the quicksort algorithm
+     * @param arr array of stings
+     * @param lo lower bound of array
+     * @param hi upper bound of array
+     */
     static void qs(String[] arr, int lo, int hi) {
         if (lo < hi) {
             int partIndex = myPartition(arr, lo, hi);
@@ -46,6 +60,14 @@ public class Main {
             qs(arr, partIndex + 1, hi);
         }
     }
+
+    /**
+     * Splits the array into two halves after comparing the values of the two strings passed in
+     * @param arr array of strings
+     * @param lo lower bound
+     * @param mid middle of array (calculated in ms function)
+     * @param hi upper bound of array
+     */
     static void merge(String[] arr, int lo, int mid, int hi)  {
         int left = mid - lo + 1;
         int right = hi - mid;
@@ -84,6 +106,13 @@ public class Main {
             k++;
         }
     }
+
+    /**
+     * Recursively calls itself until the base array is reached, then merges all arrays in order
+     * @param arr array of strings to be passed in
+     * @param lo lower bound of array
+     * @param hi upper bound of array (n-1)
+     */
     static void ms(String[] arr, int lo, int hi) {
         if (lo < hi) {
             int mid = floorDiv(lo+hi, 2);
@@ -93,13 +122,21 @@ public class Main {
         }
     }
     public static void main(String[] args) throws IOException, ArrayIndexOutOfBoundsException {
+        //Reads in a filename to take the data from
         Scanner input = new Scanner(System.in);
         //String fileIn = "sample10.txt";
-        String fileOut = "output.txt";
+        //String fileOut = "output.txt";
         System.out.println ("Enter a file to be read: ");
         String fileIn = input.nextLine();
-         //System.out.println ("Enter a name for the first output file: ");
-         //String fileOut = input.nextLine();
+
+         System.out.println ("Enter a name for the first output file: ");
+         String fileOut = input.nextLine();
+        System.out.println ("Enter a name for the second output file: ");
+        String fileOut2 = input.nextLine();
+        //String fileOut2 = "output2.txt";
+        File myFile2 = new File(fileOut2);
+
+        //Creates a reader to read the number of lines in the file
         BufferedReader reader;
         reader = new BufferedReader(new FileReader(fileIn));
         String line = reader.readLine();
@@ -109,6 +146,9 @@ public class Main {
             line = reader.readLine();
          }
         reader.close();
+
+        //Initializes two arrays of strings based on the file input
+        // lines for quicksort and mslines for merge sort
         String[] lines = new String[numlines];
         String[] mslines = new String[numlines];
         BufferedReader reader2;
@@ -118,21 +158,30 @@ public class Main {
             mslines[i] = lines[i];
         }
         reader2.close();
+
+
 //        for (int i = 0; i < numlines; i++) {
 //            System.out.println(mslines[i]);
 //        }
         int length = lines.length - 1;
+
+        //times how long the quicksort function takes
         long qsStartTime = System.nanoTime();
         //qs(lines, 0, length);
         long qsEndTime = System.nanoTime();
         long qsDuration = (qsEndTime-qsStartTime); // time taken to execute quicksort in nanoseconds
+
+        //times how long the mergesort function takes
         long msStartTime = System.nanoTime();
         ms(mslines, 0 , numlines-1);
         long msEndTime = System.nanoTime();
         long msDuration = (msEndTime-msStartTime); //time taken to execute merge sort in nanoseconds
-         for (int i = 0; i < numlines; i++) {
-             System.out.println(mslines[i]);
-         }
+
+//         for (int i = 0; i < numlines; i++) {
+//             System.out.println(mslines[i]);
+//         }
+
+        //creates a file for output and writes the sorted array from quicksort
         File myFile = new File(fileOut);
         BufferedWriter myWriter = new BufferedWriter(new FileWriter(fileOut));
         for (int i = 0; i < numlines; i++) {
@@ -142,10 +191,6 @@ public class Main {
          myWriter.flush();
         System.out.print("Time taken to execute Quick Sort (in nanoseconds): ");
         System.out.println(qsDuration);
-        //System.out.println ("Enter a name for the second output file: ");
-        //String fileOut2 = input.nextLine();
-        String fileOut2 = "output2.txt";
-        File myFile2 = new File(fileOut2);
          myWriter = new BufferedWriter(new FileWriter(fileOut2));
         for (int i = 0; i  < numlines; i++) {
             myWriter.write(mslines[i] + "\n");
